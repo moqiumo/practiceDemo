320192964e1b80109f78dd5e1a0ea3bec4ec8e3c
moqiumo <992259738@qq.com>
2020-06-03 23:01:30 +0800

    登录day1

---
+++ src/components/Login.vue
@@ -0,0 +1,108 @@
+<template>
+    <div class="login_comtainer">
+        <div class="login_box">
+            <!-- 头像区域 -->
+            <div class="avatar_box">
+                <img src="../assets/logo.png" alt="">
+            </div>
+            <!-- 登录表单 -->
+            <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRelus" label-width="0px" class="login_from">
+                <!-- 用户名 -->
+                <el-form-item prop="userName">
+                    <el-input v-model="loginForm.userName" prefix-icon="iconfont icon-user"></el-input>
+                </el-form-item>
+                <!-- 密码 -->
+                <el-form-item prop="passWord">
+                    <el-input v-model="loginForm.passWord" prefix-icon="iconfont icon-3702mima" type="password"></el-input>
+                </el-form-item>
+                <el-form-item class="btns" >
+                    <el-button type="primary" @click="login">登陆</el-button>
+                    <el-button type="info" @click="resetLoginForm">重置</el-button>
+                </el-form-item>
+            </el-form>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      loginForm: {
+        userName: 'zs',
+        passWord: 123456
+      },
+      loginFormRelus: {
+        userName: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ],
+        passWord: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 6 到 10 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    //   点击重置密码
+    resetLoginForm () {
+      this.$refs.loginFormRef.resetFields()
+    },
+    login () {
+      this.$refs.loginFormRef.validate(valid => {
+        console.log(valid)
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login_comtainer {
+    background-color: #eee;
+    height: 100%;
+}
+
+.login_box {
+    width:450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 5px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+
+    .avatar_box {
+        height: 130px;
+        width: 130px;
+        border: 1px solid #eee;
+        border-radius: 50%;
+        padding: 10px;
+        position: absolute;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        box-shadow: 0 0 10px #ddd;
+        img {
+            width: 100%;
+            height: 100%;
+            border-radius: 50%;
+            background-color: #eee;
+        }
+    }
+}
+
+.login_from {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+}
+
+.btns {
+    display: flex;
+    justify-content: flex-end;
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 导入全局样式
+import './assets/css/global.css'
+import './plugins/element.js'
 // 手动配置Element-ui
-import ElementUI from 'element-ui'
+// import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-Vue.use(ElementUI)
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+import axios from 'axios'
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
 
-
+// Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
