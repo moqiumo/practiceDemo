48f139149ba9e9a37b4eff6078ceb397377b8e8a
moqiumo <992259738@qq.com>
2020-06-07 21:21:37 +0800

    完成登录退出功能

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -8,12 +8,12 @@
             <!-- 登录表单 -->
             <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRelus" label-width="0px" class="login_from">
                 <!-- 用户名 -->
-                <el-form-item prop="userName">
-                    <el-input v-model="loginForm.userName" prefix-icon="iconfont icon-user"></el-input>
+                <el-form-item prop="username">
+                    <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
                 </el-form-item>
                 <!-- 密码 -->
-                <el-form-item prop="passWord">
-                    <el-input v-model="loginForm.passWord" prefix-icon="iconfont icon-3702mima" type="password"></el-input>
+                <el-form-item prop="password ">
+                    <el-input v-model="loginForm.password " prefix-icon="iconfont icon-3702mima" type="password"></el-input>
                 </el-form-item>
                 <el-form-item class="btns" >
                     <el-button type="primary" @click="login">登陆</el-button>
@@ -28,17 +28,17 @@ export default {
   data () {
     return {
       loginForm: {
-        userName: 'zs',
-        passWord: 123456
+        username: 'admin',
+        password: '123456'
       },
       loginFormRelus: {
-        userName: [
+        username: [
           { required: true, message: '请输入用户名', trigger: 'blur' },
-          { min: 3, max: 5, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
         ],
-        passWord: [
+        password: [
           { required: true, message: '请输入密码', trigger: 'blur' },
-          { min: 3, max: 10, message: '长度在 6 到 10 个字符', trigger: 'blur' }
+          { min: 6, max: 10, message: '长度在 6 到 10 个字符', trigger: 'blur' }
         ]
       }
     }
@@ -49,8 +49,15 @@ export default {
       this.$refs.loginFormRef.resetFields()
     },
     login () {
-      this.$refs.loginFormRef.validate(valid => {
-        console.log(valid)
+      this.$refs.loginFormRef.validate(async valid => {
+        if (!valid) return
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        // console.log(res)
+        if (res.meta.status !== 200) return this.$message.error('登陆失败')
+        this.$message.success('登陆成功')
+        // 将登陆成功token保存到客户端的sessionStorage
+        window.sessionStorage.setItem('token', res.data.token)
+        this.$router.push('/home')
       })
     }
   }
@@ -59,50 +66,49 @@ export default {
 
 <style lang="less" scoped>
 .login_comtainer {
-    background-color: #eee;
-    height: 100%;
+  background-color: #eee;
+  height: 100%;
 }
 
 .login_box {
-    width:450px;
-    height: 300px;
-    background-color: #fff;
-    border-radius: 5px;
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 5px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+  .avatar_box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
     position: absolute;
     left: 50%;
-    top: 50%;
     transform: translate(-50%, -50%);
-
-    .avatar_box {
-        height: 130px;
-        width: 130px;
-        border: 1px solid #eee;
-        border-radius: 50%;
-        padding: 10px;
-        position: absolute;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        box-shadow: 0 0 10px #ddd;
-        img {
-            width: 100%;
-            height: 100%;
-            border-radius: 50%;
-            background-color: #eee;
-        }
+    box-shadow: 0 0 10px #ddd;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
     }
+  }
 }
 
 .login_from {
-    position: absolute;
-    bottom: 0;
-    width: 100%;
-    padding: 0 20px;
-    box-sizing: border-box;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
 }
 
 .btns {
-    display: flex;
-    justify-content: flex-end;
+  display: flex;
+  justify-content: flex-end;
 }
-
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -12,11 +13,21 @@ const routes = [
   {
     path: '/',
     redirect: 'Login'
+  },
+  {
+    path: '/home',
+    component: Home
   }
 ]
 
 const router = new VueRouter({
   routes
 })
-
+// 挂在路由导航守卫 next() 表示放行 next('/login')表示强制跳转
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') return next()
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+})
 export default router
